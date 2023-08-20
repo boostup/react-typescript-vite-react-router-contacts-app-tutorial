b19ab4da51c111f26d676c9ac1436fdb797c2ea2
boostup <boostup@gmail.com>
2023-08-21 00:49:58 +0200

    feat: adding new contact using the Root route action feature, by submitting form post data

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,10 @@
-import { Link, Outlet, useLoaderData } from "react-router-dom";
-import { getContacts } from "../contacts";
+import { Link, Outlet, useLoaderData, Form } from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+
+export async function action() {
+    const contact = await createContact();
+    return { contact };
+}
 
 export default function Root() {
     const { contacts } = useLoaderData();
@@ -26,9 +31,9 @@ export default function Root() {
                             aria-live="polite"
                         ></div>
                     </form>
-                    <form method="post">
+                    <Form method="post">
                         <button type="submit">New</button>
-                    </form>
+                    </Form>
                 </div>
                 <nav>
                     {contacts.length ? (
