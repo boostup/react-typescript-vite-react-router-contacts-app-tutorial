0b2ceb2428710de9017df9b9c758080a92fbd623
boostup <boostup@gmail.com>
2023-08-21 19:43:07 +0200

    fix: UX issue when you click the back browser arrow after a search, the form field still has the value you entered even though the list is no longer filtered. In other words: Synchronized input value with the URL Search Params

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,6 +6,7 @@ import {
     redirect,
     useNavigation
 } from "react-router-dom";
+import { useEffect } from "react";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
@@ -23,6 +24,10 @@ export async function loader({ request }) {
 export default function Root() {
     const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);
 
     return (
         <>
