ec9a359b58cf546a9e914d2dacef8446e79bee68
boostup <boostup@gmail.com>
2023-08-21 16:25:35 +0200

    feat: Contact edition can now be canceled

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,6 +1,10 @@
-import { Form, useLoaderData } from "react-router-dom";
+import {
+    Form,
+    useLoaderData,
+    redirect,
+    useNavigate
+} from "react-router-dom";
 import { updateContact } from "../contacts";
-import { redirect } from "react-router-dom";
 
 export async function action({ request, params }) {
     const formData = await request.formData();
@@ -11,6 +15,7 @@ export async function action({ request, params }) {
 
 export default function EditContact() {
     const { contact } = useLoaderData();
+    const navigate = useNavigate();
 
     return (
         <Form method="post" id="contact-form">
@@ -60,7 +65,12 @@ export default function EditContact() {
             </label>
             <p>
                 <button type="submit">Save</button>
-                <button type="button">Cancel</button>
+                <button
+                    type="button"
+                    onClick={() => {
+                        navigate(-1);
+                    }}
+                >Cancel</button>
             </p>
         </Form>
     );
